ef7769fa81e27c12b62c5086a7bf8248a2f6a7f9
PedroSousa <rochapedro2009@hotmail.com>
2022-11-21 10:43:42 -0300

    Change color palette

--- App/src/components/Sidebar.tsx
+++ App/src/components/Sidebar.tsx
@@ -12,9 +12,9 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <Avatar src="https://github.com/maykbrito.png" />
+        <Avatar src="https://github.com/psousadev.png" />
 
-        <strong>Diego Fernandes</strong>
+        <strong>Pedro S</strong>
         <span>Web Developer</span>
       </div>
 
